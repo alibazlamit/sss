1c4071b76351fac43fc6742a7043ab06e9a938d6
Ali Bazlamit <ali.bazlamit@hotmail.com>
2025-07-28 22:56:03 +0200

    code review

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://alibazlamit.github.io',
-  base: '/sss2',
+  base: '/sss',
   trailingSlash: 'ignore'
 });
